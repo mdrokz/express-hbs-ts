acb0ce20872358859ec520bc3c0063d926828064
mdrokz <33598169+mdrokz@users.noreply.github.com>
2022-01-23 15:18:02 +0530

    fetch brokers

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,4 +1,26 @@
-$(document).ready(function () {
+$(document).ready(async function () {
+
+    const broker_req = await fetch('http://localhost:3000/index/brokers');
+
+    const brokers = await broker_req.json();
+
+    console.log(brokers);
+
+    const a = [];
+
+    brokers.reduce((p, c) => {
+        console.log(p, c);
+
+        // let [lcount, lcount2] = [p.listing_count, c.listing_count];
+        // let [lmonth, lmonth2] = [p.listing_month, c.listing_month];
+        // let [revnue, revenue2] = [p.avg_revenue, c.avg_revenue];
+
+        if (c.broker_id == p.broker_id) {
+            
+        }
+
+        return c;
+    })
 
     const labels = [
         "January",
@@ -22,7 +44,12 @@ $(document).ready(function () {
             backgroundColor: "rgb(255, 99, 132)",
             borderColor: "rgb(255, 99, 132)",
             data: [0, 10, 5, 2, 20, 30, 45, 80, 87, 10, 45, 56],
-        }, ],
+        }, {
+            label: "My Second dataset",
+            backgroundColor: "rgb(255, 99, 120)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45, 80, 17, 10, 35, 40],
+        }],
     };
 
     const config = {
